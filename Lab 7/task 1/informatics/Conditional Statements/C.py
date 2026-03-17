00fcc0f0015d6b0017183ff3d7bfaf47dd847e38
k_abukhair <kairbekabulhair173@gmail.com>
2026-03-17 23:07:42 +0500

    Lab 7

---
+++ Lab 7/task 1/informatics/Conditional Statements/C.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+
+if a == 1:
+    if b == 1:
+        print("YES")
+    else:
+        print("YES")
+else:
+    if b == 1:
+        print("NO")
+    else:
+        print("YES")
